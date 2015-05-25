68593110d963b540d4f7ca289fe42f841de70ffd
julianjorgensen <julian@goldenchai.com>
2015-05-25 14:05:38 -0700

    tabs, buttons and such

--- _source/_assets/js/controllers.js
+++ _source/_assets/js/controllers.js
@@ -5,6 +5,37 @@ avenueApp.controller('marqueeAccordionCtrl', ['$scope', function($scope) {
 
 // INDEX CONTROLLER
 avenueApp.controller('indexController', ['$scope', function($scope) {
+}]);
+
+
+// TABS CONTROLLER
+avenueApp.controller('tabsController', ['$scope', function ($scope){
+  this.tab = 1;
+
+  this.selectTab = function (setTab){
+  	this.tab = setTab;
+  };
+  this.isSelected = function(checkTab) {
+  	return this.tab === checkTab;
+  };
+}]);
+
+
+// BUTTONS CONTROLLER
+avenueApp.controller('choicesController', ['$scope', function ($scope){
+  this.selectChoice = function (setChoice){
+  	this.choice = setChoice;
+  };
+  this.isSelected = function(checkChoice) {
+  	return this.choice === checkChoice;
+  };
+  this.anyIsSelected = function() {
+    return (this.choice === 1 || this.choice === 2 || this.choice === 3);
+  };
+}]);
+
+
+avenueApp.controller('buttonSelector', ['$scope', function($scope) {
 
 }]);
 
