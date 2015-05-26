b762f24454387440750c941431c46a7351854fcd
julianjorgensen <julian@goldenchai.com>
2015-05-26 14:58:24 -0700

    implemented "Activate content" attribute for choices buttons directive

--- _source/_assets/js/directives.js
+++ _source/_assets/js/directives.js
@@ -1,28 +1,31 @@
 // CHOICES BUTTONS DIRECTIVE
 // ============================
 avenueApp.directive('choices', function() {
-  return {
-    restrict: 'E',
-    templateUrl: '/pages/templates/choices.html',
-    scope: {
-      first: '@',
-      firstDescription: '@',
-      second: '@',
-      secondDescription: '@',
-      third: '@',
-      thirdDescription: '@'
-    },
-    controller: function($scope) {
-      $scope.selectChoice = function (setChoice){
-      	this.choice = setChoice;
-        console.log('testa');
-      };
-      $scope.isSelected = function(checkChoice) {
-      	return this.choice === checkChoice;
-      };
-      $scope.anyIsSelected = function() {
-        return (this.choice === 1 || this.choice === 2 || this.choice === 3);
-      };
-    }
-  };
+    return {
+        restrict: 'E',
+        templateUrl: '/pages/templates/choices.html',
+        scope: {
+            first: '@',
+            firstDescription: '@',
+            second: '@',
+            secondDescription: '@',
+            third: '@',
+            thirdDescription: '@',
+            activateContent: '@'
+        },
+        controller: function($scope) {
+            $scope.selectChoice = function (setChoice){
+                this.choice = setChoice;
+                if ($(this.activateContent).hasClass('hide')){
+                    $(this.activateContent).show();
+                }
+            };
+            $scope.isSelected = function(checkChoice) {
+                return this.choice === checkChoice;
+            };
+            $scope.anyIsSelected = function() {
+                return (this.choice === 1 || this.choice === 2 || this.choice === 3);
+            };
+        }
+    };
 });
