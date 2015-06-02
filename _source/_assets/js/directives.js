598ca6852e805fc1cf6ac5f01c9e2fbddd47d7a2
julianjorgensen <julian@goldenchai.com>
2015-06-02 16:01:21 -0700

    first milestone completed: typography, general modules (styles and js), variables, layout and placeholder images

--- _source/_assets/js/directives.js
+++ _source/_assets/js/directives.js
@@ -3,19 +3,47 @@
 avenueApp.directive('choices', function() {
     return {
         restrict: 'E',
-        templateUrl: '../pages/templates/choices.html',
         scope: {
+            question: '@',
             first: '@',
             firstDescription: '@',
+            firstImage: '@',
             second: '@',
             secondDescription: '@',
+            secondImage: '@',
             third: '@',
             thirdDescription: '@',
-            activateContent: '@'
+            thirdImage: '@',
+            activateContent: '@',
+            template: '@',
+            align: '@',
+            currentImage: '@',
+            marqueeBleed: '@',
+            choicesColor: '@'
+        },
+        templateUrl: function(elem,attrs) {
+          if (attrs.templateUrl){
+            return 'pages/templates/' + attrs.templateUrl + '.html';
+          }else{
+            return 'pages/templates/choices-horizontal.html';
+          }
+        },
+        link: function(scope, element, attrs){
+          scope.currentImage = scope.firstImage;
         },
         controller: function($scope) {
             $scope.selectChoice = function (setChoice){
                 this.choice = setChoice;
+
+                // change the image accordingly
+                if (setChoice == '1'){
+                  this.currentImage = this.firstImage;
+                }else if (setChoice == '2'){
+                  this.currentImage = this.secondImage;
+                }else{
+                  this.currentImage = this.thirdImage;
+                }
+
                 if ($(this.activateContent).hasClass('hide')){
                     $(this.activateContent).show();
                 }
@@ -28,4 +56,15 @@ avenueApp.directive('choices', function() {
             };
         }
     };
+});
+
+
+
+
+// choices buttons directive
+avenueApp.directive('choicesButtons', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'pages/templates/choices-buttons.html'
+  }
 });
